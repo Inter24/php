c3bfbf76710050945b914cf012fe2bc32e3be891
Inter24 <web@bk-company.ru>
2020-11-13 15:24:34 +0300

    Add numbers to calc

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -4,9 +4,34 @@ let operation = null;
 const inputWindow = document.getElementById('inputWindow');
 
 
+
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+for (let index = 0; index < numbers.length; ++index) {
+
+    document.getElementById('btn_'+ numbers[index]).addEventListener('click', function () {
+        if (inputWindow.value === '0') {
+            inputWindow.value = numbers[index];
+        } else {
+            inputWindow.value += numbers[index];
+        }     
+    })
+
+}
+/*
+document.getElementById('btn_1').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '1';
+    } else {
+        inputWindow.value += '1';
+    }     
+})*/
+
+
+
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
-    inputWindow.value = '';
+    inputWindow.value = '0';
 })
 
