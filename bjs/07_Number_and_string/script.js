18a360b1386def1cf50a33b28f3d6b7ccea7f9e7
Inter24 <web@bk-company.ru>
2020-11-13 15:59:25 +0300

    Main operations

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -3,8 +3,6 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 
-
-
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 for (let index = 0; index < numbers.length; ++index) {
@@ -13,25 +11,78 @@ for (let index = 0; index < numbers.length; ++index) {
         if (inputWindow.value === '0') {
             inputWindow.value = numbers[index];
         } else {
-            inputWindow.value += numbers[index];
-        }     
-    })
+            inputWindow.value += numbers[index];       
+        }
+     })
 
 }
-/*
-document.getElementById('btn_1').addEventListener('click', function () {
-    if (inputWindow.value === '0') {
-        inputWindow.value = '1';
-    } else {
-        inputWindow.value += '1';
-    }     
-})*/
 
-
+// ****************** OPERATIONS *********************
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '0';
 })
+document.getElementById('btn_plus').addEventListener('click', function () {
+    if (inputWindow.value != '') {
+        lastOperand = parseInt(inputWindow.value);
+        operation = 'sum';
+        inputWindow.value = '';
+    }
+})
+document.getElementById('btn_minus').addEventListener('click', function () {
+    if (inputWindow.value != '') {
+        lastOperand = parseInt(inputWindow.value);
+        operation = 'minus';
+        inputWindow.value = '';
+    }
+})
+document.getElementById('btn_mnog').addEventListener('click', function () {
+    if (inputWindow.value != '') {
+        lastOperand = parseInt(inputWindow.value);
+        operation = 'mnog';
+        inputWindow.value = '';
+    }
+})
+document.getElementById('btn_del').addEventListener('click', function () {
+    if (inputWindow.value != '') {
+        lastOperand = parseInt(inputWindow.value);
+        operation = 'del';
+        inputWindow.value = '';
+    }
+})
 
+// ****************** RESULT *********************
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+
+    if (lastOperand !='' && inputWindow.value !='') {
+
+        if (operation === 'sum') {
+            var result = lastOperand + parseInt(inputWindow.value);
+            document.querySelector('.log').innerHTML += lastOperand + ' + ' + inputWindow.value + ' = ' + result + '<br>'; 
+            inputWindow.value = result;
+        }  
+
+        if (operation === 'minus') {
+            var result = lastOperand - parseInt(inputWindow.value);
+            document.querySelector('.log').innerHTML += lastOperand + ' - ' + inputWindow.value + ' = ' + result + '<br>'; 
+            inputWindow.value = result;
+        }   
+
+        if (operation === 'mnog') {
+            var result = lastOperand * parseInt(inputWindow.value);
+            document.querySelector('.log').innerHTML += lastOperand + ' * ' + inputWindow.value + ' = ' + result + '<br>'; 
+            inputWindow.value = result;
+        }
+
+        if (operation === 'del') {
+            var result = lastOperand / parseInt(inputWindow.value);
+            document.querySelector('.log').innerHTML += lastOperand + ' / ' + inputWindow.value + ' = ' + result + '<br>'; 
+            inputWindow.value = result;
+        }
+
+    }
+
+})
